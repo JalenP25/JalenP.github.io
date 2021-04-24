7783e7573ea61b5f03bc2e76d2f623d4dfb4a009
Jalen Peeler <jalenpeeler@yahoo.com>
2021-04-23 23:57:46 -0400

    updates to all

--- script.js
+++ script.js
@@ -9,9 +9,17 @@ function getQuote() {
     return fetch('https://animechan.vercel.app/api/random')
     .then(response => response.json())
     .then(data => {
-        // anime.innerHTML = data.anime;
+        anime.innerHTML = data.anime;
         quote.innerHTML = `"${data.quote}"`;
         character.innerHTML = data.character;
-        
     })
     }
+
+let count = 0;
+const colors = ["#00ffff", "#ffff00", "#ff00ff"];
+const changeColor = () => {
+  count++;
+  if (count >= colors.length) count = 0;
+  document.querySelector('.holder').style.background = colors[count];
+}
+document.getElementById("btn").addEventListener("click", changeColor);
